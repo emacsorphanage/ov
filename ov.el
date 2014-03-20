fe4d6cdfefbec0684ef2163563b8714c99366686
ShingoFukuyama <ShingoFukuyama@users.noreply.github.com>
2014-03-20 15:11:20 +0900

    Add read-only for overlay

--- ov.el
+++ ov.el
@@ -41,41 +41,41 @@
 (defun ov-match (string)
   (save-excursion
     (goto-char (point-min))
-    (let (ovs)
+    (let (ov-or-ovs)
       (overlay-recenter (point-max))
       (while (search-forward string nil t)
-        (setq ovs (cons (make-overlay (match-beginning 0) (match-end 0)) ovs)))
-      ovs)))
+        (setq ov-or-ovs (cons (make-overlay (match-beginning 0) (match-end 0)) ov-or-ovs)))
+      ov-or-ovs)))
 
 (defun ov-regexp (regexp)
   (save-excursion
     (goto-char (point-min))
-    (let (ovs)
+    (let (ov-or-ovs)
       (overlay-recenter (point-max))
       (while (re-search-forward regexp nil t)
-        (setq ovs (cons (make-overlay (match-beginning 0) (match-end 0)) ovs)))
-      ovs)))
+        (setq ov-or-ovs (cons (make-overlay (match-beginning 0) (match-end 0)) ov-or-ovs)))
+      ov-or-ovs)))
 
-(defun ov-map-set (ovs properties)
-  (or (listp ovs) (setq ovs (cons ovs nil)))
+(defun ov-map-set (ov-or-ovs properties)
+  (or (listp ov-or-ovs) (setq ov-or-ovs (cons ov-or-ovs nil)))
   (unless (evenp (length properties))
     (error "Error: invalid properties pairs"))
   (mapc (lambda (ov)
           (let ((props properties))
             (while props
               (overlay-put ov (pop props) (pop props)))))
-        ovs)
+        ov-or-ovs)
   nil)
 (defalias 'ov-map-put 'ov-map-set)
 
-(defmacro ov-map-clear (ovs)
-  (or (listp ovs) (setq ovs (cons ovs nil)))
+(defmacro ov-map-clear (ov-or-ovs)
+  (or (listp ov-or-ovs) (setq ov-or-ovs (cons ov-or-ovs nil)))
   `(progn
      (mapc (lambda (ov)
              (delete-overlay ov))
-           ,ovs)
-     (if (symbolp ',ovs)
-         (setq ,ovs nil))))
+           ,ov-or-ovs)
+     (if (symbolp ',ov-or-ovs)
+         (setq ,ov-or-ovs nil))))
 
 ;;(setq aaq (ov-regexp "def"))
 ;;(setq aaq (ov-match "def"))
@@ -112,12 +112,12 @@
   (list (overlay-start ov) (overlay-end ov)
         (overlay-buffer ov) (overlay-properties ov)))
 
-(defun ov-map-spec (ovs)
-  (or (listp ovs) (setq ovs (cons ovs nil)))
+(defun ov-map-spec (ov-or-ovs)
+  (or (listp ov-or-ovs) (setq ov-or-ovs (cons ov-or-ovs nil)))
   (mapcar (lambda (ov)
             (list (overlay-start ov) (overlay-end ov)
                   (overlay-buffer ov) (overlay-properties ov)))
-          ovs))
+          ov-or-ovs))
 
 (defalias 'ov-recenter 'overlay-recenter)
 
@@ -155,12 +155,72 @@
     (save-excursion
       (goto-char (or point (point)))
       (setq o (make-overlay (point-at-bol) (min (1+ (point-at-eol)) (point-max))))
-      (ov-map-set o properties))))
+      (when properties
+        (ov-map-set o properties)))
+    o))
 ;; (ov-line-at '(face helm-match))
 ;; (ov-line-at '(face helm-match) 4000)
 ;; (ov-timeout 0.6 '(ov-line-at '(face helm-match)) '(ov-clear-all))
 
+(setq ooo (ov-line-at))
 
+(ov-read-only ooo)
+
+
+
+
+(defun ov-read-only (ov-or-ovs)
+  (or (listp ov-or-ovs) (setq ov-or-ovs (cons ov-or-ovs nil)))
+  (mapc (lambda (ov)
+          (overlay-put ov 'modification-hooks    '(ov--read-only))
+          (overlay-put ov 'insert-in-front-hooks '(ov--read-only)))
+        ov-or-ovs))
+
+;; Impliment read-only overlay function
+;; http://lists.gnu.org/archive/html/emacs-devel/2002-08/msg00428.html
+(defvar ov-save-comint-last-prompt-overlay nil)
+(defun ov--read-only (overlay after start end &optional len)
+  (if (and (not after)
+           (or (< (overlay-start overlay) start)
+               (> (overlay-end overlay) end)))
+      (error "")))
+
+(defadvice comint-output-filter (around swap-read-only activate)
+  "Add a read-only equivalency to the last prompt overlay."
+  ;; Caution: in Emacs <~21.2, a new overlay gets created for each
+  ;; prompt... in later versions, text-properties for old prompts
+  ;; are used instead, and the original overlay is recycled.  In
+  ;; this case, we can advise snapshot-last-prompt to remove the
+  ;; read-only *text properties* (not the overlay properties).
+  ;; Here we test to ensure the prompt isn't in the same position as
+  ;; the process-mark before removing the read-only overlay stuff.
+  (when (and ov-save-comint-last-prompt-overlay
+             (not (equal
+                   (marker-position (process-mark (get-buffer-process
+                                                   (current-buffer))))
+                   (overlay-end
+                    ov-save-comint-last-prompt-overlay))))
+    (overlay-put ov-save-comint-last-prompt-overlay
+                 'modification-hooks nil)
+    (overlay-put ov-save-comint-last-prompt-overlay
+                 'insert-in-front-hooks' nil))
+  ad-do-it
+  (when comint-last-prompt-overlay
+    (setq ov-save-comint-last-prompt-overlay
+          comint-last-prompt-overlay)
+    (overlay-put comint-last-prompt-overlay 'intangible t)
+    (overlay-put comint-last-prompt-overlay 'modification-hooks
+                 '(idlwave-shell-comint-signal-read-only))
+    (overlay-put comint-last-prompt-overlay 'insert-in-front-hooks
+                 '(idlwave-shell-comint-signal-read-only))))
+
+(defadvice comint-snapshot-last-prompt (after remove-text-read-only activate)
+  "Remove the read-only text properties potentially set by snapshot"
+  (when comint-last-prompt-overlay
+    (remove-text-properties
+     (overlay-start comint-last-prompt-overlay)
+     (overlay-end comint-last-prompt-overlay)
+     '(modification-hooks nil insert-in-front-hooks nil))))
 
 
 
